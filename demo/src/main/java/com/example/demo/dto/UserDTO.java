09d7c856ea86dbd5bdcd461b537e2b37cf18226b
chanidu-n <chaniduneerada@gmail.com>
2025-05-27 12:22:50 +0530

    constructors

--- demo/src/main/java/com/example/demo/dto/UserDTO.java
+++ demo/src/main/java/com/example/demo/dto/UserDTO.java
@@ -9,5 +9,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserDTO {
     private int id;
-    private String username;
+    private String name;
 }
--- demo/src/main/java/com/example/demo/model/User.java
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -1,18 +1,45 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
+
 @Data
 public class User {
     @Id
+    @Column(name = "test")
     private int id;
     private String name;
+
+
+    public User() {
+    }
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
 
