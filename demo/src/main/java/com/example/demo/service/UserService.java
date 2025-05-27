8197dbdb637bfefbb132efc60089aa1798b227f0
chanidu-n <chaniduneerada@gmail.com>
2025-05-27 10:38:49 +0530

    ok

---
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,27 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.UserDTO;
+import com.example.demo.model.User;
+import com.example.demo.repo.UserRepo;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.modelmapper.TypeToken;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserService {
+    @Autowired
+   private UserRepo userRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<UserDTO> getAllUsers() {
+        List<User>userList = userRepo.findAll();
+        return modelMapper.map(userList, new TypeToken(){}.getType());
+    }
+}
